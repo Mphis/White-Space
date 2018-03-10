f0edf508d75f143fedaba080b523de02332589bb
Mphis <ppch09@gmail.com>
2018-03-10 16:36:19 +0530

    Update WSpace.js

--- WSpace.js
+++ WSpace.js
@@ -1,24 +1,83 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();/*var drawing =( function() {
-		var context,
-			canvas;
+if(window.addEventListener) {
+window.addEventListener('load', function () {
+  var canvas, context, tool;
 
+  function init () {
+    // Find the canvas element.
+    canvas = document.getElementById('imageView');
+    if (!canvas) {
+      alert('Error: I cannot find the canvas element!');
+      return;
+    }
 
-    init= function ()	
+    if (!canvas.getContext) {
+      alert('Error: no canvas.getContext!');
+      return;
+    }
 
-	{
-	
-		canvas=document.getElementById('whitecanvas');
+    // Get the 2D canvas context.
+    context = canvas.getContext('2d');
 
-		context=document.getElementById('whitecanvas').getContext("2d");
-		context.beginPath(); 
-		context.font = "30px Arial";
-		context.fillText("Hello World",100,50);
-  	}
+    // Pencil tool instance.
+    draw = new drawing();
 
-	}
+    // Attach the mousedown, mousemove and mouseup event listeners.
+    canvas.addEventListener('mousedown', ev_canvas, false);
+    canvas.addEventListener('mousemove', ev_canvas, false);
+    canvas.addEventListener('mouseup',   ev_canvas, false);
+  }
 
-	)*/
+  // This painting tool works like a drawing pencil which tracks the mouse
+  // movements.
+  function drawing () {
+    var draw = this;
+    this.started = false;
+
+    // This is called when you start holding down the mouse button.
+    // This starts the pencil drawing.
+    this.mousedown = function (ev) {
+        context.beginPath();
+        context.moveTo(ev._x, ev._y);
+        draw.started = true;
+    };
+
+    // This function is called every time you move the mouse. Obviously, it only
+    // draws if the tool.started state is set to true (when you are holding down
+    // the mouse button).
+    this.mousemove = function (ev) {
+      if (draw.started) {
+        context.lineTo(ev._x, ev._y);
+        context.stroke();
+      }
+    };
+
+    // This is called when you release the mouse button.
+    this.mouseup = function (ev) {
+      if (tool.started) {
+        draw.mousemove(ev);
+        draw.started = false;
+      }
+    };
+  }
+
+  // The general-purpose event handler. This function just determines the mouse
+  // position relative to the canvas element.
+  function ev_canvas (ev) {
+    if (ev.layerX || ev.layerX == 0) { // Firefox
+      ev._x = ev.layerX;
+      ev._y = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) { // Opera
+      ev._x = ev.offsetX;
+      ev._y = ev.offsetY;
+    }
+
+    // Call the event handler of the tool.
+    var func = draw[ev.type];
+    if (func) {
+      func(ev);
+    }
+  }
+
+  init();
+
+}, false); }
