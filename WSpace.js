ec506266aed107b8f24dea4fa617bbed802166f2
Mphis <ppch09@gmail.com>
2018-03-11 10:58:03 +0530

    Update WSpace.js

--- WSpace.js
+++ WSpace.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function () {
   var drawwith;
   var color = 'yellowgreen';
   var plots = [];
+  var isActive = false;
 
 
   function whitespace (ev) {
@@ -12,8 +13,8 @@ window.addEventListener('load', function () {
       ev._x = ev.layerX;
       ev._y = ev.layerY;
     }
-    var x = e.offsetX || e.layerX - canvas.offsetLeft;
-    var y = e.offsetY || e.layerY - canvas.offsetTop;
+    var x = e.offsetX - canvas.offsetLeft;
+    var y = e.offsetY - canvas.offsetTop;
     drawOnCanvas(plots);
 
 
