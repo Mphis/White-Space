ba4054b0cb07896c8583b6222e70badd6d79ad4d
Mphis <ppch09@gmail.com>
2018-03-11 10:20:12 +0530

    Update WSpace.js

--- WSpace.js
+++ WSpace.js
@@ -1,24 +1,19 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();/*var drawing =( function() {
-		var context,
-			canvas;
-
-
-    init= function ()	
-
-	{
-	
-		canvas=document.getElementById('whitecanvas');
-
-		context=document.getElementById('whitecanvas').getContext("2d");
-		context.beginPath(); 
-		context.font = "30px Arial";
-		context.fillText("Hello World",100,50);
-  	}
-
-	}
-
-	)*/
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Board</title>
+		<style>
+			#container { position: relative; }
+			#whiteboard { border: 1px solid #000; }
+		</style>
+	</head>
+	<body>
+		<div id="container">
+			<canvas id="whiteboard" width="720" height="480">
+			</canvas>
+		</div>
+		<script src="https://cdn.pubnub.com/pubnub-3.15.2.min.js"></script>
+		<script src="board.js"></script>
+	</body>
+</html>
